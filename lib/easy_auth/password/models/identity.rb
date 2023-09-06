817f1b7fba9a682019de1cc96742e07e66633fab
jack <jack@nustechnology.com>
2023-09-06 14:59:18 +0700

    fix: type_cast_for_schema

--- lib/easy_auth/password/models/identity.rb
+++ lib/easy_auth/password/models/identity.rb
@@ -10,7 +10,7 @@ module EasyAuth::Password::Models::Identity
   #
   # @param [Boolean] value
   def remember=(value)
-    @remember = ::ActiveRecord::Type::Boolean.new.type_cast_from_database(value)
+    @remember = ::ActiveRecord::Type::Boolean.new.type_cast_for_schema(value)
   end
 
   # Generates a new remember token and updates it on the identity record
